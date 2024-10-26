acf8b80258fd157612d8a7694297a20d1ecae05f
DanilsMIT <danilsmitdm@gmail.com>
2024-10-26 16:19:22 -0500

    Excepcion, configuracion WEBXML, ConexionBDD,Convertidor fecha, packetes

---
+++ evaluacionfinal/src/main/java/com/krakedev/moduloii/evaluacionfinal/excepciones/InventarioException.java
@@ -0,0 +1,7 @@
+package com.krakedev.moduloii.evaluacionfinal.excepciones;
+
+public class InventarioException extends Exception{
+	public InventarioException(String errorMensaje) {
+		super(errorMensaje);
+	}
+}
